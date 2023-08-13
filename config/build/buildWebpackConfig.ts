0e4a1851a565c580992f69baa3fabb0c5d24a0d5
RomanMitaki <r.mitaki@yandex.ru>
2023-08-13 18:29:52 +0300

    add: webpack dev-server, env-variables

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,13 +3,15 @@ import webpack from "webpack";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode: mode,
         entry: paths.entry,
+        devtool: isDev ? 'inline-source-map' : undefined,
         output: {
             filename: "[name].[contenthash].js",
             path: paths.build,
@@ -20,5 +22,6 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {someFn} from "./test";
 
 someFn(5);
+
+document.body.innerHTML = `<di style="background: greenyellow">Hello world!</div>`
