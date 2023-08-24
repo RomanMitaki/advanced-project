8017878d4b27db51df9cd06003346c8e225a6e27
RomanMitaki <r.mitaki@yandex.ru>
2023-08-24 22:12:49 +0300

    add: i18n setup for translation

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -9,7 +9,10 @@ declare module '*.scss' {
 
 declare module "*.png"
 declare module "*.jpeg"
+declare module "*.jpg"
 declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
+
+declare const __IS_DEV__: boolean;
