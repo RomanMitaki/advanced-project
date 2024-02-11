c5824287b6b810165e402c44f1be67fa25bcec12
RomanMitaki <r.mitaki@yandex.ru>
2024-02-11 21:17:11 +0300

    add redux toolkit (StoreProvider)

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,19 @@
+import { type ReactNode } from 'react'
+import { Provider } from 'react-redux'
+import { createReduxStore } from 'app/providers/StoreProvider/config/store'
+import { type StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
+
+interface StoreProviderProps {
+  children?: ReactNode
+  initialState?: StateSchema
+}
+
+export const StoreProvider = ({ children, initialState }: StoreProviderProps) => {
+  const store = createReduxStore(initialState)
+
+  return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+  )
+}
